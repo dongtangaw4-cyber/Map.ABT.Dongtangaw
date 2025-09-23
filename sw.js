6e6ecdaef6879531720585cbc6178d8dcaac26c4
dongtangaw4-cyber <dongtangaw4@gmail.com>
2025-09-23 16:03:17 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const VERSION = 'v13';                       // ← เปลี่ยนเลขทุกครั้งที่อัป
+const VERSION = 'v14';                       // ← เปลี่ยนเลขทุกครั้งที่อัป
 const STATIC_CACHE = `abt-static-${VERSION}`;
 
 const STATIC_ASSETS = [
