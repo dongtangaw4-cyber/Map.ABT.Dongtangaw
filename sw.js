dff5dcafb80d89f7ec1687b1e05a49d9f57accaf
dongtangaw4-cyber <dongtangaw4@gmail.com>
2025-10-02 14:54:15 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const VERSION = 'v16';                       // ← เปลี่ยนเลขทุกครั้งที่อัป
+const VERSION = 'v17';                       // ← เปลี่ยนเลขทุกครั้งที่อัป
 const STATIC_CACHE = `abt-static-${VERSION}`;
 
 const STATIC_ASSETS = [
@@ -67,4 +67,5 @@ self.addEventListener('message', (event) => {
   }
 
 });
+
 
